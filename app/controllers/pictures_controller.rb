cdbc4985ceba0c949584becb0473be7d8524f202
hidakago <hidakago@gmail.com>
2019-08-20 12:45:10 +0900

    ログイン機能の作り込み

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,9 @@
 class PicturesController < ApplicationController
   def index
-    @pictures = Picture.all
+    if logged_in?
+      @pictures = Picture.all
+    else
+      redirect_to new_session_path
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+  def new
+    if logged_in?
+      redirect_to pictures_path
+    end
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to pictures_path
+    else
+      flash[:danger] = 'ログインに失敗しました'
+      render 'new'
+    end
+  end
 end
