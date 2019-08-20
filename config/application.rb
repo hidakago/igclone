cdb7ca3420f5504c2dfd81eb9c3b864db88685f2
hidakago <hidakago@gmail.com>
2019-08-20 12:07:25 +0900

    トップページ(ログイン画面)の仮作成、アカウント登録画面の仮作成、投稿一覧画面の仮作成、アカウント登録機能の作り込み、config/application.rbの記述修正

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Igclone
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
-    config.i18n.default_locale = :ja
+    # config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
