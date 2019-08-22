8b10f438ef06a7be7e050c11e94bef1257e0bd21
hidakago <hidakago@gmail.com>
2019-08-22 23:52:59 +0900

    ユーザ情報画面の追加変更、プロフィール編集画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,29 @@ class UsersController < ApplicationController
     @pictures = @user.pictures.all.order(:updated_at).reverse_order
   end
 
+  def edit
+    if logged_in?
+      @user = User.find(params[:id])
+    else
+      redirect_to new_session_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params2)
+      redirect_to user_path(current_user.id), notice: "プロフィール情報の編集が完了しました！"
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def user_params2
+    params.require(:user).permit(:name,  :email, :comment, :image, :image_cache)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
                     uniqueness: true
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password, presence: true, length: { minimum: 6 }, on: :update, allow_blank: true
   has_many :pictures
   has_many :favorites, dependent: :destroy
   has_many :favorite_pictures, through: :favorites, source: :picture
+  mount_uploader :image, ImageUploader
 end
